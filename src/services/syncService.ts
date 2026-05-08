380105a2378ba66b6a2c1fcf8a36b701221ec7e1
danilonovaisv <danilo_novais@yahoo.com.br>
2026-05-07 23:56:29 -0300

    eniar mudanças

--- src/services/syncService.ts
+++ src/services/syncService.ts
@@ -391,7 +391,7 @@ async function fetchAllPages<T>(
 ): Promise<T[]> {
   const all: T[] = [];
   let from = 0;
-  // eslint-disable-next-line no-constant-condition
+   
   while (true) {
     const { data, error } = await query([from, from + pageSize - 1]);
     if (error) throw new Error(error.message);
